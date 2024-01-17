4b008d109e607a4731f0c32427075adec0fefa57
Jacobo149 <jtp323@lehigh.edu>
2024-01-16 22:42:14 -0500

    BugFix

--- hardhat.config.js
+++ hardhat.config.js
@@ -2,7 +2,7 @@ require("@nomicfoundation/hardhat-toolbox");
 
 // Go to https://alchemy.com, sign up, create a new App in
 // its dashboard, and replace "KEY" with its key
-const ALCHEMY_API_KEY = "hgFDEN5qNntUsCPBnTQiRzS8RPYvY5i6"; // Replace this with your Alchemy key
+const ALCHEMY_API_KEY = "API Key"; // Replace this with your Alchemy key
 
 // Replace this private key with your Sepolia account private key
 // To export your private key from Coinbase Wallet, go to
@@ -10,7 +10,7 @@ const ALCHEMY_API_KEY = "hgFDEN5qNntUsCPBnTQiRzS8RPYvY5i6"; // Replace this with
 // To export your private key from Metamask, open Metamask and
 // go to Account Details > Export Private Key
 // Beware: NEVER put real Ether into testing accounts
-const SEPOLIA_PRIVATE_KEY = "<Private-Key>";
+const SEPOLIA_PRIVATE_KEY = "Private key";
 
 module.exports = {
   solidity: "0.8.23",
