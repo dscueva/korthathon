dc047b4e2a6e034800f577e31092b6f37c3adfa5
“dscueva” <dscueva@gmail.com>
2024-01-18 10:44:01 -0500

    updated deploy to interact with the new smart contract's parameters

--- scripts/interact.js
+++ scripts/interact.js
@@ -1,7 +1,7 @@
 const { ethers } = require("hardhat");
 
 async function main() {
-    const contractAddress = "0x162B8271d22a75Ef7564aB7869E35b29789BB2A3"; // Updated contract address
+    const contractAddress = "0x1CBC9bccc786845651b89d198127CBa181E61314"; // Updated contract address
     const walletAddress = "0x5bEb07c71Da8ceD22A392847E775a245c4F431de"; // Your wallet address
     const [signer] = await ethers.getSigners();
 
@@ -9,7 +9,7 @@ async function main() {
     const ghoMultisig = await GhoMultisig.attach(contractAddress);
 
     // Query the token balance of your wallet address
-    const tokenBalance = await ghoMultisig.getTokenBalance(walletAddress);
+    const tokenBalance = await ghoMultisig.balanceGHO();
     const tokenDecimals = 18; // Replace with the actual decimal value of your token
 
     // Convert the token balance to a string and then parse it as a decimal
