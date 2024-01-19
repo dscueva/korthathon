dfc88caf5bb723de69a84d8694ed68e3f0be936f
Jacobo149 <jtp323@lehigh.edu>
2024-01-18 20:05:59 -0500

    Bug in submitTransaction

--- scripts/contract_functions.js
+++ scripts/contract_functions.js
@@ -8,7 +8,7 @@ async function balanceGHO(ghoMultisig, walletAddress) {
     // Convert the token balance to a string and then parse it as a decimal
     const adjustedTokenBalance = parseFloat(tokenBalance.toString()) / 10 ** tokenDecimals;
 
-    console.log(`Token balance of address ${walletAddress}: ${adjustedTokenBalance} GHO`);
+    console.log(`Token balance of Contract: ${adjustedTokenBalance} GHO`);
 }
 
 async function balanceETH(ghoMultisig, walletAddress) {
@@ -18,18 +18,38 @@ async function balanceETH(ghoMultisig, walletAddress) {
     // Convert the token balance to a string and then parse it as a decimal
     const adjustedTokenBalance = parseFloat(tokenBalance.toString()) / 10 ** tokenDecimals;
 
-    console.log(`Ether balance of address ${walletAddress}: ${adjustedTokenBalance} ETH`);
+    console.log(`Ether balance of Contract: ${adjustedTokenBalance} ETH`);
 }
 
-// Function to deposit GHO into contract
-//async function depositGHO(ghoMultisig, walletAddress, amount) {
-//    const deposit = await ghoMultisig.depositGHO(amount);
-//    console.log(`Deposited ${amount} GHO to ${walletAddress}`);
-//}
+// Add a parameter for the wallet instance
+async function submitTransaction(ghoMultisig, destinationAddress, amount, walletWithProvider) {
+    try {
+        // Create a transaction
+        const unsignedTx = await ghoMultisig.populateTransaction.submitTransaction(destinationAddress, amount);
+
+        // Sign the transaction with the existing wallet
+        const signedTx = await walletWithProvider.signTransaction(unsignedTx);
+
+        // Send the signed transaction
+        const txResponse = await walletWithProvider.provider.sendTransaction(signedTx);
+
+        console.log(`Transaction signed and sent: ${txResponse.hash}`);
+        // Wait for the transaction to be mined
+        await txResponse.wait(1);
+        console.log(
+            `Transaction has been mined at block number: ${txResponse.blockNumber}, transaction hash: ${txResponse.hash}`
+        );
+    } catch (error) {
+        console.error("Error in submitTransaction:", error.message);
+    }
+}
+
+
+
 
 module.exports = {
     balanceGHO,
     balanceETH,
-    //depositGHO
+    submitTransaction
     
 };
