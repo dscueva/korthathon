e3272be683a3166961a81f1ec103505281003ac7
“dscueva” <dscueva@gmail.com>
2024-01-19 15:29:22 -0500

    added user input

--- scripts/main.js
+++ scripts/main.js
@@ -1,17 +1,17 @@
-// main.js
 const { ethers } = require("hardhat");
+const readlineSync = require('readline-sync');
 const sdk = require("./ghoMultiSigSDK.js");
 
 async function main() {
-    const contractAddress = "0x6f0f025f9a53f66825Ea96370C60077f8b869bBd";
-    const ghoTokenAddress = "0xc4bF5CbDaBE595361438F8c6a187bDc330539c60";
-    const recipientAddress = "0xc977Fdb84F4ed2425f6afA5f47bd686291615451";
-    const amountGho = ethers.parseUnits("69", 18);
+    const contractAddress = readlineSync.question('Enter contract address: ');
+    const recipientAddress = readlineSync.question('Enter recipient address: ');
+    const amountGhoInput = readlineSync.question('Enter amount of GHO tokens: ');
+    const amountGho = ethers.parseUnits(amountGhoInput, 18);
 
     const signer = await sdk.getSigner();
-    await sdk.approveGhoTokens(signer, ghoTokenAddress, contractAddress, amountGho);
+    await sdk.approveGhoTokens(signer, "0xc4bF5CbDaBE595361438F8c6a187bDc330539c60", contractAddress, amountGho);
     await sdk.depositGhoTokens(signer, contractAddress, amountGho);
-    await sdk.getMultisigGhoBalance(signer, ghoTokenAddress, contractAddress);
+    await sdk.getMultisigGhoBalance(signer, "0xc4bF5CbDaBE595361438F8c6a187bDc330539c60", contractAddress);
     await sdk.submitTransaction(signer, contractAddress, recipientAddress, amountGho);
 
     // Assuming you want the last transaction
